d447400c12e5a7123167ed816b4daebcfe264601
Eric Vidal <evidal@MacBook-Pro-de-Eric.local>
2014-12-12 06:48:06 +0100

    Initial commit, in a first step the goal is to implement a basic version of Logistic Regression in Java. Then, when it will works, I will try to plus it on Elastic Search.

---
+++ src/main/java/com/elasticlearning/utils/MatrixIOUtils.java
@@ -0,0 +1,22 @@
+package com.elasticlearning.utils;
+
+import cern.colt.matrix.tdouble.impl.DenseDoubleMatrix2D;
+
+import java.io.File;
+
+/**
+ * Eric Vidal
+ * Date: 11/12/14
+ */
+public class MatrixIOUtils {
+    static DenseDoubleMatrix2D loadFromFile(File location) {
+        DenseDoubleMatrix2D m = null;
+
+
+        return m;
+    }
+
+    static void writeToFile(File location)  {
+        throw new UnsupportedOperationException("Not Yet Implented");
+    }
+}
